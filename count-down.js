a793fb8d83d92ac35e7b17a1da56a078a42dcfbb
pylvrms <pverms18@gmail.com>
2025-01-29 14:39:02 +0530

    Add files via upload

---
+++ count-down.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $('[data-countdown]').each(function () {
+        var deadline = moment($(this).data('countdown')).valueOf();
+        var datadays = $(this).children('[data-days]');
+        var datahours = $(this).children('[data-hours]');
+        var dataminutes = $(this).children('[data-minutes]');
+        var dataseconds = $(this).children('[data-seconds]');
+
+        var x = setInterval(function () {
+            var now = new Date().getTime();
+            var t = deadline - now;
+
+            var days = Math.floor(t/(1000*60*60*24));
+            var hours = Math.floor(t%(1000*60*60*24) / (1000*60*60));
+            var minutes = Math.floor(t%(1000*60*60) / (1000*60));
+            var seconds = Math.floor(t%(1000*60) / (1000));
+
+            datadays.html(days < 10 ? '0'+days:days).append('<span class="label">Days</span>');
+            datahours.html(hours < 10 ? '0'+hours:hours).append('<span class="label">Hours</span>');
+            dataminutes.html(minutes < 10 ? '0'+minutes:minutes).append('<span class="label">Mins</span>');
+            dataseconds.html(seconds < 10 ? '0'+seconds:seconds).append('<span class="label">Secs</span>');
+
+            if(t <= 0){
+                clearInterval(x);
+                datadays.html('0').append('<span class="label">Days</span>');
+                datahours.html('0').append('<span class="label">Hours</span>');
+                dataminutes.html('0').append('<span class="label">Mins</span>');
+                dataseconds.html('0').append('<span class="label">Secs</span>');
+            }
+        },1000);
+    });
+});
+
+
